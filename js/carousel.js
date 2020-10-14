49b754a23a5d1f43a556dae1a781b54229e25458
karthibabu <54367776+karthibabu10@users.noreply.github.com>
2020-10-14 07:47:21 +0530

    Changed Testimonial
    Changed Testimonial

---
+++ js/carousel.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function($) {
+    "use strict";
+    //  TESTIMONIALS CAROUSEL HOOK
+    $('#customers-testimonials').owlCarousel({
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots:true,
+        autoplayTimeout: 8500,
+        smartSpeed: 450,
+        responsive: {
+          0: {
+            items: 1
+          },
+          768: {
+            items: 2
+          },
+          1170: {
+            items: 3
+          }
+        }
+    });
+});
